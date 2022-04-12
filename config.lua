35c896f551d9fd3e478ed463dc8896d52f2438b2
K A L A S H <74117936+Kalassh@users.noreply.github.com>
2022-04-13 01:28:42 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,9 +1,11 @@
-Babica = {}
+Config = {}
 
 -- Ukljuci/Iskljuci qtarget
-Babica.qtarget = false
+Config.qtarget = false
 
--- Vrijeme ozivljavanja (sekunde)
-Babica.vrijeme = 30
-
+Config.Babica = {
+    Lokacija1 = vec3(-53.6833, -2523.3218, 7.4012),
+    Lokacija2 = vec3(-440.4526, 1595.0342, 358.4680),
+    Vrijeme = 60, -- Vrijeme ozivljavanja (sekunde)
+}
 
